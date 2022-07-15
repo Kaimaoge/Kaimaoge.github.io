803c5aeec91f34390f888ba4c814c5b4644527f5
Yuankai Wu <Kaimaogege@gmail.com>
2022-07-15 16:18:32 +0800

    Update table_header.js

--- table_header.js
+++ table_header.js
@@ -1,1 +1,27 @@
+document.write("<table width=\"650\" border=\"0\" height=\"184\">");
+document.write("<tbody>");
+document.write("<tr>");
+document.write("<td> <img src=\"Images/wu.jpg\" alt=\"Yuankai Wu\"");
+document.write("width=\"162.6\" height=\"200\"> <\/td>");
+document.write("<td>");
+document.write("<h1>Yuankai Wu</h1>");
+document.write("<br>");
+document.write("<b>Professor</b>");
+document.write("<br>");
+
+document.write("<a href=\"https:\/\/cs.scu.edu.cn\/info\/1279\/17062.htm">Sichuan University<\/a></b>");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<b>Contact info:</b><br>");
+document.write(" Chengdu, Sichuan, China. <br>");
+document.write("E-mail: wuyk0@scu.edu.cn <br>");
+document.write("<br>");
+document.write("&nbsp;");
+document.write("<a href=\"https:\/\/scholar.google.com\/citations?user=L9b-8ZwAAAAJ&hl=en\"><img src=\"Images\/scholar.png\" alt=\"Google Scholar\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<a href=\"https:\/\/github.com\/Kaimaoge\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<\/td>");
+document.write("<\/tr>");
+document.write("<\/tbody>");
+document.write("<\/table>");
 
